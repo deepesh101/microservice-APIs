7b4d3437fba3eb9b508522f704ee3db85865613a
Deepesh Gupta <deepeshg4615@gmail.com>
2022-03-12 02:57:39 +0530

    Home Route added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,11 @@ module.exports = app => {
     require('./user')(app)
     require('./content')(app)
     require('./daily_pass')(app)
+    app.get('/', async (req, res, next) => {
+        try {
+            res.send("API is Working")
+        } catch (e) {
+            next(e)
+        }
+    })
 }
